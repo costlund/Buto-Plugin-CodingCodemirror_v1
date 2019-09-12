9bbb99c8929c5f16f3ca4efe8835a7dfbd853c54
Claes Ö <>
2019-09-12 23:39:30 +0200

    Improvement.

--- PluginCodingCodemirror_v1.php
+++ PluginCodingCodemirror_v1.php
@@ -10,12 +10,16 @@ class PluginCodingCodemirror_v1{
     wfPlugin::includeonce('wf/array');
     $data = new PluginWfArray($data);
     $id = $data->get('data/id');
+    $mode = null;
     if($data->get('data/mode')=='yml'){
       $mode = new PluginWfYml(__DIR__.'/mode/yml.yml');
       $mode = json_encode($mode->get());
     }elseif($data->get('data/mode')=='php'){
       $mode = new PluginWfYml(__DIR__.'/mode/php.yml');
       $mode = json_encode($mode->get());
+    }
+    if(!$mode || !$id){
+      exit('Error in settings.');
     }
     $element = new PluginWfYml(__DIR__.'/element/apply.yml');
     $script = "var htmlEditor = CodeMirror.fromTextArea(document.getElementById('$id'),  $mode ).on('change', editor => {document.getElementById('$id').value=editor.getValue();});";
